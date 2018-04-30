5a822bfce7ab4b7a0570176e9abfccb049748e0d
Juan Bigorra <juanchoibf@gmail.com>
2018-04-30 19:51:28 +0200

    Moved files into the scripts folder

--- baby-steps.js
+++
@@ -1,5 +0,0 @@
-const numArr = process.argv.splice(2);
-
-const sum = numArr.reduce((acc, value) => acc + (+value), 0);
-
-console.log(sum);
--- my-first-io.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-const filePath = process.argv[2] || 'eol.txt';
-const file = fs.readFileSync(filePath);
-const eolNumber = file.toString().split('\n').length;
-console.log(eolNumber - 1);
